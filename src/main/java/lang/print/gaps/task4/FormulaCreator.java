0d4085af83c61079dc90fcaf3713d38fd3fefcec
arastepa <stepanyan2298@gmail.com>
2022-12-20 15:18:45 +0400

    ..

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task4;
 
+import javax.swing.plaf.synth.SynthStyle;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        double res = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4*a*b) / 2);
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c';
+        int h = 'h';
+        int a = 'a';
+        int r = 'r';
+        System.out.println(c + "\n" + h + "\n" + a +"\n" + r + "\n");
+
     }
 }
